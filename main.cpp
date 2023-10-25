9fbb97339cd3a5345cd1974ddd0164b52acfe48e
dirii <minhquang116@gmail.com>
2023-10-26 01:40:08 +0700

    create basic structure for Raylib

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "_raygui.h"
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
---
+++ source/_raygui.h
@@ -0,0 +1,14 @@
+//
+// Created by dirii on 26/10/23.
+//
+
+#ifndef CROSSING_ROAD__RAYGUI_H
+#define CROSSING_ROAD__RAYGUI_H
+
+// NOTE: This file is only for raygui implementation purposes because RAYGUI_IMPLEMENTATION should be called only once
+// This file is used in main.cpp for implementing raygui
+#include "raylib.h"
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
+#endif //CROSSING_ROAD__RAYGUI_H
