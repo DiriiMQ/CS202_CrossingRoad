4c627b47c1c2bf98e635161bbedd381a2b5554a7
dirii <minhquang116@gmail.com>
2023-10-28 22:40:06 +0700

    add json parse

---
+++ source/Window.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dirii on 26/10/23.
+//
+
+#include "Window.h"
---
+++ source/Window.h
@@ -0,0 +1,14 @@
+//
+// Created by dirii on 26/10/23.
+//
+
+#ifndef CROSSING_ROAD_WINDOW_H
+#define CROSSING_ROAD_WINDOW_H
+
+
+class Window {
+
+};
+
+
+#endif //CROSSING_ROAD_WINDOW_H
